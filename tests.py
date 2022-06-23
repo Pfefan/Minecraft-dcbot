6a0935ca88b2308d03e524b6a12b7fd9edb99c98
Pfefan <stefanpfeiffer04@gmail.com>
2022-06-23 15:21:12 +0200

    added tests, vscode settings

---
+++ tests.py
@@ -0,0 +1,17 @@
+"""Module imports"""
+
+import editdatabase
+dbmanager = editdatabase.Databasemanager()
+
+def test_plyhistoryaddrem():
+    """Test playerhistory db"""
+    dbmanager.plyhistoryadd("testserver")
+    data = dbmanager.plyhistoryall()
+    assert "testserver" in data
+
+    dbmanager.plyhistoryremove("testserver")
+    data = dbmanager.plyhistoryall()
+    assert "testserver" not in data
+
+
+            
